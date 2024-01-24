ab9b5c4c40f835ff3655e774a5078027b3f7b9ec
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-01-24 18:42:10 +0100

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './paginas/general/home/home.component';
+import { ContactComponent } from './paginas/general/contact/contact.component';
+import { AboutComponent } from './paginas/general/about/about.component';
+import { SigninComponent } from './paginas/general/signin/signin.component';
+import { NotfoundComponent } from './paginas/general/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    SigninComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
